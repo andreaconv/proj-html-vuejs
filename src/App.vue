5570a166559f1168d7f233010d418e1da0164aa7
andreaconv <andrea2convertini@gmail.com>
2023-05-09 12:31:31 +0200

    [add] macrostruttura

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,39 @@
 <script>
+import Header from './components/myHeader.vue';
+import Main from './components/myMain.vue';
+import Footer from './components/myFooter.vue';
+import { store } from './data/store';
+
 export default {
   name: 'App',
+  components: {
+    Header,
+    Main,
+    Footer,
+  },
+  data(){
+    return {
+      store,
+    }
+  },
+  methods: {
+    show(){
+      store.showString = 'Hello'
+      console.log(store.showString)
+    }
+  },
+  mounted(){
+    this.show()
+  }
 }
 </script>
 
 <template>
-  <h1>APP</h1>
+
+  <Header />
+  <Main />
+  <Footer />
+
 </template>
 
 <style lang="scss">
---
+++ src/components/myFooter.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  name: 'Footer',
+}
+</script>
+
+<template>
+  <h1>FOOTER</h1>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
