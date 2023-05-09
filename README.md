d4aedaa51962c4c119075a426de68420657d579b
andreaconv <andrea2convertini@gmail.com>
2023-05-09 11:59:45 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# PROJ HTML VUEJS
+
+## CONSEGNA 
+
+Riprodurre il layout proposto solo in versione Desktop
+https://maxcoach.thememove.com/main/modern-schooling/
---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  name: 'App',
+}
+</script>
+
+<template>
+  <h1>APP</h1>
+</template>
+
+<style lang="scss">
+  @use './scss/main.scss';
+</style>
